1764db10c108cf6e864a1e8728a3cafeea596fea
Italianasavitri <italiana.savitri@gmail.com>
2019-03-05 14:54:28 +0700

    Revisi Submit Master

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -17,7 +17,8 @@ export class AppConstant {
       // this.SERVER_URL = 'http://10.14.153.41:1366';
       // this.SERVER_URL = 'http://localhost:1366';
       // icofr
-      this.SERVER_URL = 'http://localhost:9092';
+      // this.SERVER_URL = 'http://localhost:9092';
+      this.SERVER_URL = 'http://10.14.22.209:8080/erkap-webservice';
     } else {
       this.SERVER_URL = document.location.protocol + '//' +
         document.location.hostname + ':' +
